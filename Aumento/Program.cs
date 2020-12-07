dd6c5b1eae5e6e42bb142a033a3a4f765af1ba97
Elaine Maria <emsilva.elaine@gmail.com>
2020-12-07 16:13:31 -0300

    projeto Aumento

---
+++ Aumento/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+namespace Aumento {
+    class Program {
+        static void Main(string[] args) {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double Salario, Porcentagem, Aumento, Novosalario; 
+
+            Console.WriteLine("Digite o salario da pessoa:");
+            Salario = double.Parse(Console.ReadLine(), CI);
+
+            if (Salario < 1000.0) {
+                Porcentagem = 20;
+            }
+            else if (Salario > 1000.0 && Salario < 3000.0) {
+                Porcentagem = 15;
+            }
+            else if (Salario > 3000.0 && Salario < 8000.0) {
+                Porcentagem = 10;
+            }
+            else{
+                Porcentagem = 5;
+
+            }
+
+            Aumento = Salario * Porcentagem/100;
+            Novosalario = Salario + Aumento;
+
+            Console.WriteLine("Novo salario : R$ "+Novosalario.ToString("F2", CI));
+            Console.WriteLine("Aumento : R$ " + Aumento.ToString("F2", CI));
+            Console.WriteLine("Porcentagem " + Porcentagem + "%");
+        }
+
+    }
+}
